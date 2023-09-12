053438c69631aa377e5f44d81ab0e18c1edac5a8
Yuxi Wang <yuxi.wangcd@gmail.com>
2023-09-12 15:11:00 -0700

    Update content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,9 @@
 <h1>Minimals</h1>
 
 <p>
-  Hi, there. Welcome to this small site where I learn about threejs, instanced
-  mesh, and structural geology etc..
-</p>
+  Hi, there. Welcome to this small site where I play with threejs.</p>
 
 <p>
   Feel free to checkout <a href="/instancedmesh">Instanced Mesh Example</a> and
-  <a href="structural-geology">Structural Geology Example</a>.
+  <a href="structural-geology">Structural Geology Example</a>, <a href="pointcloud">PointCloud</a>..
 </p>
